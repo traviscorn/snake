aae4218a3b331d5902f06b3e0afee6ef50d850f2
traviscorn <86335479+traviscorn@users.noreply.github.com>
2021-07-24 21:23:08 -0600

    Update game.js

--- game.js
+++ game.js
@@ -9,7 +9,7 @@ const gameBoard = document.getElementById('game-board')
 function main(currentTime) {
   if (gameOver) {
     if (confirm('You lost. Press OK to restart.')) {
-      window.location = 'snake/index.html'
+      window.location = '/index.html'
     }
     return
   }
