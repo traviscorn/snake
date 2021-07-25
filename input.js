3dd975feafe5af4120b22b347e4a559035c69a9a
traviscorn <86335479+traviscorn@users.noreply.github.com>
2021-07-24 20:54:19 -0600

    Add files via upload

---
+++ input.js
@@ -0,0 +1,86 @@
+let inputDirection = { x: 0, y: 0 }
+let lastInputDirection = { x: 0, y: 0 }
+
+window.addEventListener('keydown', e => {
+    switch (e.key) {
+        case 'ArrowUp':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: -1 }
+            break
+        case 'ArrowDown':
+            if (lastInputDirection.y !== 0) break
+            inputDirection = { x: 0, y: 1 }
+            break
+        case 'ArrowLeft':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: -1, y: 0 }
+            break
+        case 'ArrowRight':
+            if (lastInputDirection.x !== 0) break
+            inputDirection = { x: 1, y: 0 }
+            break
+    }
+})
+
+export function getInputDirection() {
+    lastInputDirection = inputDirection
+    return inputDirection
+}
+
+var container = document.querySelector('window');
+
+  window.addEventListener("touchstart", startTouch, false);
+  window.addEventListener("touchmove", moveTouch, false);
+
+  // Swipe Up / Down / Left / Right
+  var initialX = null;
+  var initialY = null;
+
+  function startTouch(e) {
+    initialX = e.touches[0].clientX;
+    initialY = e.touches[0].clientY;
+  };
+
+  function moveTouch(e) {
+    if (initialX === null) {
+      return;
+    }
+
+    if (initialY === null) {
+      return;
+    }
+
+    var currentX = e.touches[0].clientX;
+    var currentY = e.touches[0].clientY;
+
+    var diffX = initialX - currentX;
+    var diffY = initialY - currentY;
+
+    switch (Math.abs(diffX) > Math.abs(diffY)) {
+      // sliding horizontally
+      case (diffX > 0):
+        // swiped left
+        if (lastInputDirection.x !== 0) break
+        inputDirection = { x: -1, y: 0 }
+        break
+        // swiped right
+        if (lastInputDirection.x !== 0) break
+        inputDirection = { x: 1, y: 0 }
+        break; 
+      // sliding vertically
+      case (diffY > 0):
+        // swiped up
+        if (lastInputDirection.y !== 0) break
+        inputDirection = { x: 0, y: -1 }
+        break
+        // swiped down
+        if (lastInputDirection.y !== 0) break
+        inputDirection = { x: 0, y: 1 }
+        break;
+    } 
+
+    initialX = null;
+    initialY = null;
+
+    e.preventDefault();
+  };
